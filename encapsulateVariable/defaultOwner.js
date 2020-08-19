98fff84ff685eae76edf3c9707e4de0a845e0533
Kirsten Nordine <kirstennordine@gmail.com>
2020-08-18 20:16:25 -0400

    Use Encapsulate Record to prevent changes

--- encapsulateVariable/defaultOwner.js
+++ encapsulateVariable/defaultOwner.js
@@ -1,5 +1,15 @@
 let defaultOwnerData = { firstName: "Kirsten", lastName: "Nordine" };
 
-export function defaultOwner() { return Object.assign({}, defaultOwnerData); }
+export function defaultOwner() { return new Person(defaultOwnerData); }
 export function updateDefaultOwner(ownerData) { defaultOwnerData = ownerData; }
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+
+  get lastName() { return this._lastName; }
+  get firstName() { return this._firstName; }
+}
 
