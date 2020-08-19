6073f9efbb81b1108b92e277955e7cb7cfbe80d0
Kirsten Nordine <kirstennordine@gmail.com>
2020-08-18 21:08:04 -0400

    Use "Introduce Assertion" to prove that production can be calculated
    Easy way to test the hypothesis without changing too much!

--- replace-derived-variable-with-query/productionPlan.js
+++ replace-derived-variable-with-query/productionPlan.js
@@ -4,7 +4,14 @@ class ProductionPlan {
     this._production = 0;
   }
 
-  get production() { return this._production; }
+  get production() {
+    assert(this._production === this.calculatedProduction);
+    return this._production;
+  }
+
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, adjustment) => sum + adjustment.amount, 0);
+  }
 
   applyAdjustment(someAdjustment) {
     this._adjustments.push(someAdjustment);
