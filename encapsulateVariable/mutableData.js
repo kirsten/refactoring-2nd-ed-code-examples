52906dbbf32993229c8bd9f9b3adcc7f5a273600
Kirsten Nordine <kirstennordine@gmail.com>
2020-08-18 20:05:02 -0400

    Create encapsulating functions to access and update the variable
    Not used yet, but this creates a clear point to monitor changes and how
    you're using data. Example -> add a validation!

--- encapsulateVariable/mutableData.js
+++ encapsulateVariable/mutableData.js
@@ -1,5 +1,8 @@
 let defaultOwner = { firstName: "Kirsten", lastName: "Nordine" };
 
+function getDefaultOwner() { return defaultOwner; }
+function updateDefaultOwner(ownerData) { defaultOwner = ownerData; }
+
 spaceship.owner = defaultOwner;
 
 defaultOwner = { firstName: "Kelsey", lastName: "Nordine" };
